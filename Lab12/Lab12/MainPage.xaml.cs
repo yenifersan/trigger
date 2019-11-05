e04b1e6cb50982401d6a5b7e8bd60b442256e631
yenifersan <44785957+yenifersan@users.noreply.github.com>
2019-11-04 23:27:54 -0500

    Agregar archivos de proyecto.

---
+++ Lab12/Lab12/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lab12
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Item1.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Lab12.Data());
+
+            };
+            Item2.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Lab12.Event());
+            };
+
+            Item4.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Lab12.Property());
+
+            };
+            Item5.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Lab12.Style());
+
+            };
+
+            Item6.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Lab12.MultiTrigger());
+
+            };
+
+        }
+    }
+}
+
---
+++ Lab12/Lab12/MultiTrigger.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lab12
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MultiTrigger : ContentPage
+    {
+        public MultiTrigger()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
